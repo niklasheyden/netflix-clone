90ea95741dc01b37afe04b74a5f22400581a2d55
Niklas von Heyden <niklas.heyden@me.com>
2021-05-16 19:34:00 +0200

    fetch movies and tv shows from different categories and display them in scrollable rows

--- src/Requests.js
+++ src/Requests.js
@@ -3,15 +3,14 @@ const API_KEY = "4df4623ce7adaf0a5ef4ce7f728453d5";
 
 const requests = {
   fetchPopular: `/movie/popular?api_key=${API_KEY}&language=en-US&page=1`,
-  fetchTrending: `/trending/all/week?api_key${API_KEY}&language=en-US`,
-  fetchNetflixOriginals: `/discover/tv?api_key${API_KEY}&with_networks=213`,
-  fetchTopRated: `/movie/top_rated?api_key${API_KEY}&language=en-US`,
-  fetchActionMovies: `/discover/movie?api_key${API_KEY}&withgenres=28`,
-  fetchComedyMovies: `/discover/movie?api_key${API_KEY}&withgenres=35`,
-  fetchHorrorMovies: `/discover/movie?api_key${API_KEY}&withgenres=27`,
-  fetchRomanceMovies: `/discover/movie?api_key${API_KEY}&withgenres=10749`,
-  fetchDocumentaries: `/discover/movie?api_key${API_KEY}&withgenres=99`,
+  fetchTrending: `/trending/all/day?api_key=${API_KEY}`,
+  fetchNetflixOriginals: `/movie/popular?api_key=${API_KEY}&language=en-US&page=1`,
+  fetchTopRated: `/tv/top_rated?api_key=${API_KEY}&language=en-US&page=1`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&language=en-US&&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&language=en-US&&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&language=en-US&&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&language=en-US&&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&language=en-US&&with_genres=99`,
 }
 
 export default requests;
-
