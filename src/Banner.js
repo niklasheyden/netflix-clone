9a2a63c806a66e2cfb687fba46e5be7540035628
Niklas von Heyden <niklas.heyden@me.com>
2021-05-16 18:12:17 +0200

    connect tmdb api to fetch banner movie info

--- src/Banner.js
+++ src/Banner.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Banner.css';
+import axios from './axios';
+import requests from './Requests';
 
 function Banner() {
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchPopular);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length -1)
+        ]
+      );
+      return request;
+    }
+
+    fetchData();
+  }, [])
+
+  console.log(movie);
 
   function truncate(string, n) {
     return string?.length > n ? string.substr(0, n-1) + '...' : string
@@ -11,24 +30,20 @@ function Banner() {
     <header className='banner'
       style={{
         backgroundSize: "cover",
-        backgroundImage: `url("https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Black_flag.svg/1200px-Black_flag.svg.png")`,
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
         backgroundPosition: "center center",
       }}>
 
       <div className="banner__contents">
-        <h1 className='banner__title'>Movie Name</h1>
+        <h1 className='banner__title'>
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
         <div className="banner__buttons">
           <button className='banner__button'>Play</button>
           <button className='banner__button'>My List</button>
         </div>
         <h1 className="banner__description">
-          {truncate(`This is a test description This is a test descriptionThis is a test description
-                    This is a test description This is a test descriptionThis is a test description
-                    This is a test description This is a test descriptionThis is a test description
-                    This is a test description This is a test descriptionThis is a test description
-                    This is a test description sThis is a test descriptionThis is a test description`,
-                    150
-          )}
+          {truncate(movie?.overview, 150)}
 
         </h1>
       </div>
