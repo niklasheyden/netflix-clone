4247cffb603e7e7f4c51df2a768c74d614300fcf
Niklas von Heyden <niklas.heyden@me.com>
2021-05-16 15:10:24 +0200

    build nav

--- src/Nav.js
+++ src/Nav.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React , { useState, useEffect } from 'react';
 import './Nav.css';
 
 function Nav() {
+  const [show,handleShow] = useState(false)
+
+  const transitionNavBar = () => {
+    if (window.scrollY > 100) {
+      handleShow(true);
+    } else {
+      handleShow(false)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', transitionNavBar);
+    return () => window.removeEventListener('scroll', transitionNavBar)
+  }, [])
+
   return (
-    <div className='nav'>
-      <h1>This is the nav</h1>
+    <div className={`nav ${show && 'nav__black'}`}>
+      <div className="nav_contents">
+        <img
+          className='nav__logo'
+          src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+          alt="">
+        </img>
+
+        <img
+          className='nav__avatar'
+          src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/84c20033850498.56ba69ac290ea.png"
+          alt="">
+        </img>
+      </div>
     </div>
   )
 }
